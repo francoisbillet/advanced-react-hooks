48b32ce10f89ad3e9eb964830e50f19cc49c9aa2
François Billet <francoisbillet@live.fr>
2023-02-26 12:47:40 +0100

    Exercise 3 extra credit 1

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -3,20 +3,27 @@ import * as React from 'react'
 function CountProvider(props) {
   const [count, setCount] = React.useState(0)
   const value = [count, setCount]
-  return (
-    <CountContext.Provider value={value} {...props} />
-  )
+  return <CountContext.Provider value={value} {...props} /> // Or opening and closing tags with {children} in between
+}
+
+function useCount() {
+  const countCountext = React.useContext(CountContext)
+  if (!countCountext) {
+    throw new Error('useCount may only be used from within a CountProvider')
+  }
+
+  return countCountext
 }
 
 const CountContext = React.createContext()
 
 function CountDisplay() {
-  const [count] = React.useContext(CountContext)
+  const [count] = useCount()
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
-  const [, setCount] = React.useContext(CountContext)
+  const [, setCount] = useCount()
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
