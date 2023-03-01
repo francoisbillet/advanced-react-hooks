7fe364b2d7d6157a50598dbd82eb0650a1d8ec7f
François Billet <francoisbillet@live.fr>
2023-03-01 08:33:47 +0100

    Exercise 6 extra credit 1

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -1,8 +1,10 @@
 import * as React from 'react'
 
+const formatDebugValue = ({query, state}) => `${query.toString()} => ${state}`
+
 function useMedia(query, initialState = false) {
   const [state, setState] = React.useState(initialState)
-  React.useDebugValue(`${query}: ${state}`)
+  React.useDebugValue({query, state}, formatDebugValue)
 
   React.useEffect(() => {
     let mounted = true
